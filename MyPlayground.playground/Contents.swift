3c35c8505c30a6864daf92ee74e5b1070b9aedf6
ceren <cerengunnes@gmail.com>
2022-09-27 13:11:04 +0300

    day 1 is completed

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,91 @@
+import UIKit
+
+//variables
+
+//var keyword creates a new variable and it can be changed over and over again.
+
+var name = "Ted"
+name = "Rebecca"
+
+//constants
+
+//let keyword creates a new constant and it cannot be changed once it's created.
+
+let character = "Daphne"
+
+//Swift's naming convention is camelCase
+
+//strings
+//when you assign text to a constant or variable it's called a string.
+//you can use "" inside of your string as long as you put backslash before them.
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away. "
+
+//multiline strings can be made using triple double quotes on seperate lines.
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+//you can read the length of a string by writing .count after the name of the variable or constant
+print(movie.count)
+
+//uppercased() function sends back the same string except every one of its letter is uppercased.
+print(movie.uppercased())
+
+//hasPrefix() function lets us know whether a string starts with some letters of our choosing
+print(movie.hasPrefix("A day"))
+
+//strings in Swift are case-sensitive.
+
+//whole numbers - Integers(Int) which in latin means whole
+
+//create a score constant like this
+let score = 10
+
+//use _ to break up numbers however you want.
+let reallyBig = 100_000_000
+
+//this creates a counter variable equal to 10, then adds 5 more to it
+var counter = 10
+counter = counter + 5
+
+//you can use shorthand operator += which adds a number directly to the integer and it is called compound assignment operator
+counter += 5
+
+//isMultiple(of: ) function finds out whether it's a multiple of another integer.
+let number = 120
+print(number.isMultiple(of: 3))
+
+//you can use the number directly if you want
+print(120.isMultiple(of: 3))
+
+
+//Decimal numbers
+//when you create a floating-point number, Swift considers it to be a Double
+
+let num = 0.1 + 0.2
+print(num)
+
+//you cannot mix together different types(for ex: int + double will give you an error.) It is called type safety.
+
+//if you want to add a float to an integer
+let a = 1
+let b = 2.0
+let c = a + Int(b)
+//or let c = Double(a) + b
+
+//if you wanted to create a Double or an Int based on the number you provide - if there's a dot in there, you have a Double otherwise it's an Int. Even if the numbers after the dot are 0.
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+//decimal numbers have the same range of operators and compound assingment operators as integers
+var rating = 5.0
+rating *= 2
+
+
