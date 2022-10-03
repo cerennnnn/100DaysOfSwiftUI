cb6b98ec011fc7694ee69526ce69ec33d326024e
cerennnnn <cerengunnes@gmail.com>
2022-10-04 02:40:22 +0300

    day 7 part 3 is completed

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,28 +1,38 @@
 import UIKit
 
-//how to return values from functions
+//when our code can be boiled down to a single value, such as true, false, "Hello", or 19, it's called an 'expression'. Expressions are things that can be assigned to a variable, or printed using print().
+//when we're performing actions such as creating variables, starting a loop, or checking a condition, then it's called a 'statement'.
+//the expression inside there can be as long as you want, but it can't contain any statements - no loops, no conditions, no new variables, and so on.
 
-//to return your own value from a function do 2 things:
-//1st - write -> then a data type before { to tell Swift what kind of data will sent back
-//2nd - use the return keyword to send back your data
-
-//example
-func rollDice() -> Int {
-    Int.random(in: 1...6)
+func greet(name: String) -> String{
+    name == "Taylor Swift" ? "Oh wow!" : "Hello, \(name)"
 }
 
-let result = rollDice()
-print(result)
+//if we have actual if else statements omitting return keyword inside if and else is not allowed.
+func read(books: [String]) -> Bool {
+    for book in books {
+        print("I'm reading \(book)")
+    }
+    return true
+}
+read(books: ["Harry Potter 1", "Harry Potter 2"])
 
-func areLettersIdentical(string1: String, string2: String) -> Bool {
-    string1.sorted() == string2.sorted()
+//tuples
+//like arrays, dictionaries and sets, tuples lets us put multiple pieces of data into a single variable, but they are fixed in size and can have a vaariety of data types.
+
+func getUser() -> (firstName: String, lastName: String) {
+    ("Taylor", "Swift")
 }
 
-func pythagoras(a: Double, b: Double) -> Double {
-    sqrt(a * a + b * b)
-}
+let user = getUser()
+print("Name: \(user.firstName) \(user.lastName)")
 
-let c = pythagoras(a: 3, b: 4)
-print(c)
+// sometimes you’ll find you’re given tuples where the elements don’t have names. When this happens you can access the tuple’s elements using numerical indices starting from 0
+print("Name: \(user.0) \(user.1)")
 
-//if the function doesn't return a value, you can still use return by itself to force the function to exit early.
+let (firstName, lastName) = getUser()
+print("Name: \(firstName) \(lastName)")
+
+//if you don’t need all the values from the tuple you can go a step further by using _ to tell Swift to ignore that part of the tuple:
+//let (firstName, _) = getUser()
+//print("Name: \(firstName)")
