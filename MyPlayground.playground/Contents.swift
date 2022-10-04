4045dd348c61f6843e2f8f03a25824b6f419d3be
cerennnnn <cerengunnes@gmail.com>
2022-10-04 23:55:56 +0300

    day 8 part 2 is completed

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,25 +1,46 @@
 import UIKit
 
-//how to provide default values fpr parameters
+//how to handle errors in functions
 
-//if we don't specify the end value, 12 willl be used automatically.
-
-func printTimesTables(for number: Int, end: Int = 12) {
-    for i in 1...end {
-        print("\(i) x \(number) is \(i * number)")
-    }
+enum PasswordError: Error {
+    case short, obvious
 }
 
-printTimesTables(for: 5, end: 20)
-printTimesTables(for: 8)
-
-//example
-func parkCar(_ type: String, automatically: Bool = true) {
-    if automatically {
-        print("Nice - my \(type) parked itself!")
+func checkPassword(_ password: String) throws -> String {
+    if password.count < 5 {
+        throw PasswordError.short
+    }
+    
+    if password == "12345" {
+        throw PasswordError.obvious
+    }
+    
+    if password.count < 8 {
+        return "OK"
+    } else if password.count < 10 {
+        return "Good"
     } else {
-        print("I guess I'll have to do it.")
+        return "Excellent"
     }
 }
 
-print("Tesla")
+//being marked by throws doesn't mean the function must throw errors, only that it might
+//try keyword must be written before calling all functions that might throw errors, and is a visual signal to developers that regular code execution will be interrupted if an error happens.
+//when you use try, you need to be inside a do block, and make sure you have one or more catch blocks able to handle any errors.
+//you must always have a catch block that is able to handle every kind of error. However, you can also catch specific errors as well
+
+
+let string = "12345"
+
+do {
+    let result = try checkPassword(string)
+    print("Password rating: \(result)")
+} catch PasswordError.short {
+    print("Please use a longer password")
+} catch PasswordError.obvious {
+    print("I have the same combination on my luggage!")
+} catch {
+    print("There was an error .")
+}
+
+//Most errors thrown by Apple provide a meaningful message that you can present to your user if needed. Swift makes this available using an error value that's automatically provided inside your catch block, and it's common to read error.localizedDescription to see exactly what happened.
