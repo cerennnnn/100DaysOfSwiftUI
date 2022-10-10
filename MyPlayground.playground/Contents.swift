b4dd7c996b8e2ecd4fc1d8b18f4abccc33c019ed
cerennnnn <cerengunnes@gmail.com>
2022-10-10 18:51:58 +0300

    Day 9 part 3 is completed

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -19,7 +19,6 @@ let team = ["Gloria", "Suzanne", "Piper", "Tiffany", "Tasha"]
 //trailing closure syntax
 //when one function accepts another as its parameter, Swift allows special syntax called trailing closure
 
-
 let captainFirstTeam = team.sorted { name1, name2 in
     if name1 == "Suzanne" {
         return true
@@ -45,7 +44,9 @@ let captainFirstTeam1 = team.sorted {
 }
 
 //filter() lets us run some code on every item in the array and will send back a new array containing every item that returns true for the function.
+
 //we could find all team players whose name begins with T like this:
+
 let tOnly = team.filter { $0.hasPrefix("T")}
 print(tOnly)
 
@@ -57,3 +58,76 @@ print(uppercaseTeam)
 
 //When working with map(), the type you return doesn’t have to be the same as the type you started with – you could convert an array of integers to an array of strings, for example.
 
+//closures as parameters
+//example - 1
+var swift = {
+    print("Cool - I can use closures!")
+}
+
+func writeCode(using language: () -> Void) {
+    language()
+    print("That'll be eleventy billion dollar, please.")
+}
+
+writeCode(using: swift)
+
+//example - 2
+var playWithDog = {
+    print("fetch!")
+}
+
+func play(using playType: () -> Void) -> String {
+    print("Let's play a game")
+    playType()
+    return "👍"
+}
+play(using: playWithDog)
+
+//example - 3
+let awesomeTalk = {
+    print("Here's a great talk!")
+}
+
+func deliverTalk(name: String, type: () -> Void) {
+    print("My talk is called \(name)")
+    type()
+}
+
+deliverTalk(name: "My Awesome Talk", type: awesomeTalk)
+
+//example - 4
+let evilRobot = {
+    print("EXTERMINATE")
+}
+
+func buildRobot(using personality: () -> Void) {
+    print("Time to turn on the robot!")
+    personality()
+}
+
+buildRobot(using: evilRobot)
+
+//example - 5
+let resignation = { (name: String) in
+    print("Dear \(name), I'm outta here!")
+}
+
+func printDocument(contents: (String) -> Void) {
+    print("Connecting to printer... Sending document...")
+    contents("Josh")
+}
+
+printDocument(contents: resignation)
+
+//example - 6
+let driveSafely = { () -> String in
+    return "I'm being a considerate driver."
+}
+
+func drive(using driving: () -> String) {
+    print("Let's get in the car")
+    driving()
+    print("We're there!")
+}
+
+drive(using: driveSafely)
