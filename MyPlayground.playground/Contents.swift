d1fc3b806fef1b746f20330414c145f2b05d591f
cerennnnn <cerengunnes@gmail.com>
2022-10-25 11:13:09 +0300

    day12 part 4 is done

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,28 +1,35 @@
 import UIKit
 
-//how to add initializers for classes
-//if a child class has any custom initializers, it must always call the parent's initializer after it has finished setting up its own properties, if it has any.
-//Swift won't generate a memberwise initializers for classes. So you either need to write your own initializer, or provide default values for all the properties of the class.
+//How to copy classes
+//In Swift, all copies of a class shares the same data, meaning that any changes you make to one copy will automatically change the other copies. This happens because classes are reference types in Swift, which means all copies of a class all refer back to the same underlying pot of data.
 
-//let's start by defining a class
-class Vehicle {
-    let isElectric: Bool
-    
-    init(isElectric: Bool) {
-        self.isElectric = isElectric
+//to see that, let's try this:
+class User {
+    var username = "Anonymous"
+}
+//That has just one property but because it's stored inside a class it'll get shared across all copies of the class. So we can create an instance of that class:
+var user1 = User()
+//we would then take a copy of user1 and change the username value
+var user2 = user1
+user2.username = "Taylor"
+//now we've changed the copy of username property we can then print out the same properties from each different copy
+print(user1.username)
+print(user2.username)
+
+//…and that’s going to print “Taylor” for both – even though we only changed one of the instances, the other also changed.
+
+//In comparison, structs do not share their data amongst copies, meaning that if we change class User to struct User in our code we get a different result: it will print “Anonymous” then “Taylor”, because changing the copy didn’t also adjust the original.
+
+
+//If you want to create a unique copy of a class instance – sometimes called a deep copy – you need to handle creating a new instance and copy across all your data safely.
+
+class User2 {
+    var username = "Anonymous"
+    func copy() -> User2 {
+        let user = User2()
+        user.username = username
+        return user
     }
 }
+//Now we can safely call copy() to get an object with the same starting data, but any future changes won’t impact the original
 
-//That has a single Boolean property, plus an initializer to set the value for that property. Remember, using self here makes it clear we’re assigning the isElectric parameter to the property of the same name.
-
-//Now, let’s say we wanted to make a Car class inheriting from Vehicle
-class Car: Vehicle {
-    let isConvertible: Bool
-    
-    init(isConvertible: Bool, isElectric: Bool) {
-        self.isConvertible = isConvertible
-        super.init(isElectric: isElectric)
-    }
-}
-
-//super is another one of those values that Swift automatically provides for us, similar to self: it allows us to call up to methods that belong to our parent class, such as it's initializer. You can use it with other methods if you want ; it's not limited to initializers.
