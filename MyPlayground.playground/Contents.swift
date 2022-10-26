a5e85f1468ff073d0cb11e7628b0c8870751db27
cerennnnn <cerengunnes@gmail.com>
2022-10-26 15:01:29 +0300

    protocol examples are added.

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,63 +1,24 @@
 import UIKit
 
-//defining a Vehicle protocol :
-protocol Vehicle {
-    var name: String { get }
-    var currentPassengers: Int { get set }
-    func estimateTime(for distance: Int) -> Int
-    func travel(distance: Int)
+//protocol examples
+//example1
+protocol Plantable {
+    var requirements: [String] { get set }
 }
 
-//make a Car struct that conforms Vehicle protocol :
-struct Car: Vehicle {
-    let name = "Car"
-    var currentPassengers = 1
-    
-    func estimateTime(for distance: Int) -> Int {
-        distance / 50
-    }
-    
-    func travel(distance: Int) {
-        print("I'm driving \(distance)km.")
-    }
-    
-    func openSunroof() {
-        print("It's a nice day.")
-    }
+//example2
+protocol Buildable {
+    var numberOfBricks: Int { get set }
+    var materials: [String] { get set }
+}
+//It's not possible to create set-only properties in Swift.
+protocol Singable {
+    var lyrics: [String] { get set }
+    var notes: [String] { get set }
 }
 
-func commute(distance: Int, using vehicle: Vehicle) {
-    if vehicle.estimateTime(for: distance) > 100 {
-        print("That's too slow! I'll try a different vehicle.")
-    } else {
-        vehicle.travel(distance: distance)
-    }
+//example3
+protocol Purchaseable {
+    var price: Double { get set }
+    var currency: String { get set }
 }
-
-var car = Car()
-commute(distance: 100, using: car)
-
-struct Bicycle: Vehicle {
-    let name = "Bicycle"
-    var currentPassengers = 1
-    
-    func estimateTime(for distance: Int) -> Int {
-        distance / 10
-    }
-    
-    func travel(distance: Int) {
-        print("I'm cycling \(distance)km.")
-    }
-}
-
-let bike = Bicycle()
-commute(distance: 50, using: bike)
-
-func getTravelEstimates(using vehicles: [Vehicle], distance: Int) {
-    for vehicle in vehicles {
-        let estimate = vehicle.estimateTime(for: distance)
-        print("\(vehicle.name): \(estimate) hours to travel \(distance)km.")
-    }
-}
-
-getTravelEstimates(using: [car, bike], distance: 150)
