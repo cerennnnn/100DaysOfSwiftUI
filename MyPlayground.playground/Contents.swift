2865595429d5baf55609b4236916ad769f7b3ce5
cerennnnn <cerengunnes@gmail.com>
2022-10-27 16:55:53 +0300

    day14 part 4 is completed

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,46 +1,19 @@
 import UIKit
 
-//nil coalescing
+//optional chaining
 
-let captains = [
-    "Enterprise": "Picard",
-    "Voyager": "Janeway",
-    "Defiant": "Sisko"
-]
+let names = ["Arya", "Bran", "Robb", "Sansa"]
 
-//let new = captains["Serenity"]
-//let new = captains["Serenity"] ?? "N/A"
-let new = captains["Serenity", default: "N/A"]
-
-let tvShows = ["Archer", "Babylon 5", "Ted Lasso"]
-let favorite = tvShows.randomElement() ?? "None"
+let chosen = names.randomElement()?.uppercased() ?? "No one"
+print("Next in line: \(chosen)")
 
 struct Book {
-    var author: String?
-    var title: String
+    let title: String
+    let author: String?
 }
 
-let book = Book(author: nil, title: "Beowulf")
-let author = book.author ?? "Anonymous"
+var book: Book? = nil
+let author = book?.author?.first?.uppercased() ?? "A"
 print(author)
 
-let input = ""
-let number = Int(input) ?? 0
-print(number)
 
-
-//example1
-var bestPony: String? = "Pinkie Pie"
-let selectedPony: String? = bestPony ?? nil
-
-//example2
-let lightsaberColor: String? = "green"
-let color = lightsaberColor ?? "blue"
-
-//example3
-let distanceRan: Double? = 0.5
-let distance: Double = distanceRan ?? 0
-
-//example4
-var userOptedIn: Bool? = nil
-var optedIn = userOptedIn ?? false
