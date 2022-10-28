3f93685298c055905c0e5443d46a9683a2fae220
cerennnnn <cerengunnes@gmail.com>
2022-10-28 18:23:19 +0300

    day15 is completed

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,46 +1,115 @@
 import UIKit
 
-//optional chaining
-
-let names = ["Arya", "Bran", "Robb", "Sansa"]
-
-let chosen = names.randomElement()?.uppercased() ?? "No one"
-print("Next in line: \(chosen)")
-
-struct Book {
-    let title: String
-    let author: String?
+func random(arr: [Int]? ) -> Int {
+    return arr?.randomElement() ?? Int.random(in: 1...100)
 }
 
-var book: Book? = nil
-let author = book?.author?.first?.uppercased() ?? "A"
-print(author)
+random(arr: (nil))
 
-let names2 = ["Vincent": "van Gogh", "Pablo": "Picasso", "Claude": "Monet"]
-let surnameLetter = names2["Vincent"]?.first?.uppercased()
-
-//example1
-let songs: [String]? = [String]()
-let finalSong = songs?.last?.uppercased()
-
-//example2
-func albumReleased(in year: Int) -> String? {
-    switch year {
-    case 2006: return "Taylor Swift"
-    case 2008: return "Fearless"
-    case 2010: return "Speak Now"
-    case 2012: return "Red"
-    case 2014: return "1989"
-    case 2017: return "Reputation"
-    default: return nil
+class Dog {
+    var age: Int
+    
+    init(age: Int) {
+        self.age = age
     }
 }
-let album = albumReleased(in: 2006)?.uppercased()
 
-//example3
-let shoppingList = ["eggs", "tomatoes", "grapes"]
-let firstItem = shoppingList.first?.appending(" are on my shopping list")
+var dog = Dog(age: 5)
+var playfulDog = dog
+playfulDog.age = 10
 
-//example4
-let capitals = ["Scotland": "Edinburgh", "Wales": "Cardiff"]
-let scottishCapital = capitals["Scotland"]?.uppercased()
+print(dog.age, playfulDog.age)
+
+enum UIStyle {
+    case light, dark, sydstem
+}
+
+var style: UIStyle = .dark
+
+
+var albums: [String] = ["Red", "Fearless"]
+var user: [String: String] = ["id": "@twostraws"]
+var books: Set<String> = Set(["The Bluest Eye", "Foundation"])
+
+var teams: [String] = [String]()
+var clues = [String]()
+var arr: [String]
+
+var hate = ""
+var lyric = "Hater's gonna \(hate)"
+
+for _ in 1...5 {
+    lyric += "hate "
+}
+print(lyric)
+
+func getUser() -> (firstName: String, lastName: String ) {
+    (firstName: "Taylor", lastName: "Swift")
+}
+
+var name = getUser().firstName
+print(name)
+
+func upperCasse( _ string: String ) -> Bool {
+    string == string.uppercased()
+}
+upperCasse("TAYLOR")
+
+func multiply(with number: Int) {
+    for i in 1...5 {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+multiply(with: 3)
+
+struct Employee {
+    var name: String
+    var vacationAllowed = 14
+    var vacationTaken = 0
+    
+    var vacationRemaining: Int {
+        get {
+            vacationAllowed - vacationTaken
+        }
+        set {
+            vacationAllowed = vacationTaken + newValue
+        }
+    }
+}
+
+var emp = Employee(name: "Emp1")
+emp.vacationRemaining
+
+struct Game {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var game = Game()
+game.score += 10
+game.score -= 3
+
+struct Player {
+    let name: String
+    let number: Int
+    
+    init(name: String) {
+        self.name = name
+        number = Int.random(in: 1...99)
+    }
+}
+
+extension Collection {
+    var isNotEmpty: Bool {
+        isEmpty == false
+    }
+}
+
+let guests = ["Mario", "Luigi", "Peach"]
+if guests.isNotEmpty {
+    print("Guests count: \(guests.count)")
+}
