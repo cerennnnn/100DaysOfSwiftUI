5745fe04a918f02c359220d26548d102a8193cc1
cerennnnn <cerengunnes@gmail.com>
2022-10-30 21:31:42 +0300

    WeSplit app is finished

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ceren Güneş on 29.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
