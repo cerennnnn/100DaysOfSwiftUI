c77beafce96025d48b452fac3080247c5ce5ce4c
cerennnnn <cerengunnes@gmail.com>
2022-11-20 21:46:52 +0300

    .

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,15 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 0.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+    VStack {
+    Button("Tap Me") {
+        // animationAmount += 1
     }
+    .padding(50)
+    .background(.red)
+    .foregroundColor(.white)
+    .clipShape(Circle())
+    .overlay(
+        Circle()
+            .stroke(.red)
+            .scaleEffect(animationAmount)
+            .opacity((animationAmount) * 0.25)
+            .animation(
+                .easeInOut(duration: 1),
+//                    .repeatForever(autoreverses: false),
+                value: animationAmount
+            )
+    )
+    .onAppear {
+        animationAmount = 2
+    }
+    }
+}
 }
 
 struct ContentView_Previews: PreviewProvider {
