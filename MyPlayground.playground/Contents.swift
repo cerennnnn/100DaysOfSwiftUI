811536752ca8ecbbde2a47a3ed99a0069d6dd484
cerennnnn <cerengunnes@gmail.com>
2022-11-26 20:45:16 +0300

    .

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -5,3 +5,15 @@ var array = [1, 2, 3, 4, 5]
 //emoty string of type Int
 var setExample: Set<Int> = []
 Set(array)
+
+
+for i in ["1", "2"] {
+    print(i)
+}
+
+let vt = (name: "ABCC", val: 5)
+print(vt.0)
+
+"t".forEach( { char in
+print(char)
+})
