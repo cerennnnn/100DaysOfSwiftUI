dd549a3bffc65d560bee43200e1d2db204822e54
Ceren <cerengunnes@gmail.com>
2024-01-05 18:05:28 +0300

    Finished Day 5.

--- Day04-06.playground/Contents.swift
+++ Day04-06.playground/Contents.swift
@@ -26,3 +26,163 @@ print(stringArray.count)
 
 let uniquesStringArray = Set(stringArray)
 print(uniquesStringArray.count)
+
+//If - else
+let score = 85
+
+if score > 80 {
+    print("Great job!")
+}
+
+let speed = 88
+let percentage = 85
+let age = 18
+
+if speed >= 88 {
+    print("Where we're going we don't need roads.")
+}
+
+if percentage < 85 {
+    print("Sorry, you failed the test.")
+}
+
+if age >= 18 {
+    print("You're eligible to vote.")
+}
+
+let ourName = "Dave Lister"
+let friendName = "Arnold Rimmer"
+
+if ourName < friendName {
+    print("It's \(ourName) vs \(friendName)")
+}
+
+if ourName > friendName {
+    print("It's \(friendName) vs \(ourName)")
+}
+
+var numbers = [1, 2, 3]
+numbers.append(4)
+
+if numbers.count > 3 {
+    numbers.remove(at: 0)
+}
+
+print(numbers)
+
+let country = "Canada"
+
+if country == "Australia" {
+    print("G'day!")
+}
+
+//Comparable
+enum Sizes: Comparable {
+    case small
+    case medium
+    case large
+}
+
+let first = Sizes.small
+let second = Sizes.large
+print(first < second) //That will print “true”, because small comes before large in the enum case list.
+
+//Checking Multiple Conditions
+if age >= 18 {
+    print("You can vote in the next election.")
+} else {
+    print("Sorry, you're too young to vote.")
+}
+
+let temp = 25
+if temp >= 35 && temp < 30 {
+    print("It's a nice day.")
+}
+
+let userAge = 14
+let hasParentalConsent = true
+
+if age >= 18 || hasParentalConsent {
+    print("You can buy the game.")
+}
+
+enum TransportOption {
+    case airplane, helicopter, bicycle, car, escooter
+}
+
+let transport = TransportOption.airplane
+if transport == .airplane || transport == .helicopter {
+    print("Let's fly.")
+} else if transport == .bicycle {
+    print("I hope there's a bike path.")
+} else if transport == .car {
+    print("Time to get stuck in traffic.")
+} else { //escooter
+    print("I'm going to hire a scooter now!")
+}
+
+//Switch-case
+switch forecast {
+case .sun:
+    print("It should be a nice day.")
+case .rain:
+    print("Pack an umbrella.")
+case .wind:
+    print("Wear something warm")
+case .snow:
+    print("School is cancelled.")
+case .unknown:
+    print("Our forecast generator is broken!")
+}
+
+let place = "Metropolis"
+
+switch place {
+case "Gotham":
+    print("You're Batman!")
+case "Mega-City One":
+    print("You're Judge Dredd!")
+case "Wakanda":
+    print("You're Black Panther!")
+default:
+    print("Who are you?")
+}
+
+let day = 5
+print("My true love gave to me…")
+
+switch day {
+case 5:
+    print("5 golden rings")
+    fallthrough
+case 4:
+    print("4 calling birds")
+    fallthrough
+case 3:
+    print("3 French hens")
+    fallthrough
+case 2:
+    print("2 turtle doves")
+    fallthrough
+default:
+    print("A partridge in a pear tree")
+}
+
+//fallthrough -> fall through the following case.
+
+//Ternary operator
+let canVote = age >= 18 ? "Yes" : "No"
+
+let hour = 23
+print(hour < 12 ? "It's before noon" : "It's after noon")
+
+enum Theme {
+    case light, dark
+}
+
+let theme = Theme.dark
+
+let background = theme == .dark ? "black" : "white"
+print(background)
+
+
