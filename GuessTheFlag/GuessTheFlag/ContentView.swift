7ecedd3a1f23b9780d0ad910aba99948ad7180e3
Ceren <cerengunnes@gmail.com>
2024-01-19 14:54:49 +0300

    Finished GuessTheFlag.

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -12,6 +12,9 @@ struct ContentView: View {
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var score = 0
+    @State private var questionNumber = 0
+    @State private var isGameOver = false
     
     var body: some View {
         ZStack {
@@ -56,7 +59,7 @@ struct ContentView: View {
                 Spacer()
                 Spacer()
                 
-                Text("Score ???")
+                Text("Score: \(score)")
                     .foregroundStyle(.white)
                     .font(.title.weight(.bold))
                 
@@ -67,24 +70,45 @@ struct ContentView: View {
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text("Your score is \(score)")
+        }
+        
+        .alert("Game Over!", isPresented: $isGameOver) {
+            Button {
+                playAgain()
+            } label: {
+                Text("Play again")
+            }
+        } message: {
+            Text("Your final score is \(score)\nLet's start again!")
         }
     }
     
     func flagTapped(_ number: Int) {
+        questionNumber += 1
         if number == correctAnswer {
+            score += 1
             scoreTitle = "Correct!"
         } else {
-            scoreTitle = "Wrong"
+            score -= 1
+            scoreTitle = "Wrong! That's the flag of \(countries[number])"
         }
         
-        showingScore = true
+        isGameOver = questionNumber == 8 ? true : false
+        showingScore = isGameOver ? false : true
     }
     
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
-        
+    }
+    
+    func playAgain() {
+        isGameOver = false
+        showingScore = false
+        score = 0
+        questionNumber = 0
+        askQuestion()
     }
 }
 
