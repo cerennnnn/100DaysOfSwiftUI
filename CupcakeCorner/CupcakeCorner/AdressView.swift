8e4231b0be7b26e5f18db981022ca11b06e3d4e5
Ceren <cerengunnes@gmail.com>
2024-02-17 18:45:45 +0300

    Finished day 51.

---
+++ CupcakeCorner/CupcakeCorner/AdressView.swift
@@ -0,0 +1,36 @@
+//
+//  AdressView.swift
+//  CupcakeCorner
+//
+//  Created by Ceren Güneş on 16.02.2024.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    @Bindable var order: Order
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+            
+            Section {
+                NavigationLink("Check Out") {
+                    CheckoutView(order: order)
+                }
+            }
+            .disabled(order.hasValidAddress == false)
+        }
+        .navigationTitle("Delivery Details")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
