9e124e21e5684d303f4f81c3068a4cb36658f24d
Ceren <cerengunnes@gmail.com>
2024-02-19 17:36:22 +0300

    Finished Day 53.

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Ceren Güneş on 19.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
